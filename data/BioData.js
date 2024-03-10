261797347bedca648b6cff5b164d13da79295ae0
EdiValentin101 <valentinedi35@gmail.com>
2024-03-10 13:57:14 +0200

    gg

--- data/BioData.js
+++ data/BioData.js
@@ -1,13 +1,13 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'Pirate''s Cave',
+        name: "Pirate's Cave",
         username: '',
         url: '',
         titleImg: true,
         avatar: '/avatar.png',
         nftAvatar: false,
-        description: 'Pirate''s Cave started as a loud & clear response to all the scammers who stole our FREE content & started sharing it at a PRICE. We''ve recently joined the scene 3 years ago and our journey had more downs than it ever had ups, but we couldn''t turn our backs at the repulsive injustice so we came back.',
+        description: "Pirate's Cave started as a loud & clear response to all the scammers who stole our FREE content & started sharing it at a PRICE. We've recently joined the scene 3 years ago and our journey had more downs than it ever had ups, but we couldn't turn our backs at the repulsive injustice so we came back.",
         descShow: true,
         subdesc: 'Our main social media links',
         subdescShow: true,
