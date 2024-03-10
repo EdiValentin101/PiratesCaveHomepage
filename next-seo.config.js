1e53f9787379a5322b5bd48e8b6bf02cdf287af1
EdiValentin101 <valentinedi35@gmail.com>
2024-03-10 16:37:42 +0200

    gg

--- next-seo.config.js
+++ next-seo.config.js
@@ -7,6 +7,18 @@ export default {
         description: "Pirate's Cave started as a loud & clear response to all the scammers who stole our FREE content & started sharing it at a PRICE. We've recently joined the scene 3 years ago and our journey had more downs than it ever had ups, but we couldn't turn our backs at the repulsive injustice so we came back.",
         locale: 'en_EN',
         keywords: "sketchfab, sketchfab ripper, sketchfab models, how to rip sketchfab, how to download sketchfab, entirely for free, sketchfab ripping, sketchfab rip, rip sketchfab, download sketchfab, free to download, download from sketchfab, sketchfab download, models sketchfab, how to download sketchfab models for free, how to rip sketchfab models for free, free script, useful resources, support servers, click to download, against scammers, 3d model, 3d models, tampermonkey script, content",
+        images: [
+            {
+                width: 1200,
+                height: 630,
+                url: `https://vjy.me/preview.jpg`,
+            },
+        ],
         site_name: "Pirate's Cave Homepage",
+    },
+    twitter: {
+        handle: '@realvjy',
+        site: 'vjy.me',
+        cardType: 'summary_large_image',
     },
 };
