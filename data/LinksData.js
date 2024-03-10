6f8a0992e1909eb5ad93d8e32d3232ad3f6c3c9e
EdiValentin101 <valentinedi35@gmail.com>
2024-03-10 15:13:54 +0200

    gg

--- data/LinksData.js
+++ data/LinksData.js
@@ -114,7 +114,7 @@ const webLinks = [
         title: 'Support Revetix\n(Original Developer)',
         url: 'https://github.com/realvjy/nxt-lnk/generate',
         type: 'donate',
-        icon: '/revetix.png',
+        icon: '/revetix.svg',
         on: true
     },
 	/*
