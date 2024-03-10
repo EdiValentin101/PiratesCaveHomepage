b3a4f1c02723cbb80c97b43e30506f6232f3afe2
EdiValentin101 <valentinedi35@gmail.com>
2024-03-10 17:11:15 +0200

    gg

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,7 +11,7 @@ export default {
             {
                 width: 1200,
                 height: 630,
-                url: `https://vjy.me/preview.jpg`,
+                url: `/public/PiratesCaveBanner.png`,
             },
         ],
         site_name: "Pirate's Cave Homepage",
