901ac6ea3c421c603d839f2e8f2e7e2d4a3d03a3
EdiValentin101 <valentinedi35@gmail.com>
2024-03-25 20:26:11 +0200

    gg

--- data/BioData.js
+++ data/BioData.js
@@ -11,7 +11,7 @@ const bioData = [
         descShow: true,
         subdesc: 'Our main social media links',
         subdescShow: true,
-        newProductUrl: 'https://forms.gle/uRJgbyGNi45KBSAC8',
+        newProductUrl: '',
         newProduct: true,
         author: 'HoReyModZ',
         authorURL: '',
